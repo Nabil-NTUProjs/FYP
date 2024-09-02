81829c2bc812de8462fc7d5147c3b7ebc30d4469
Muhammad Nabil Hakeem <muhammad.nabilhakeem@gmail.com>
2024-09-02 16:08:01 +0800

    Delete convert_to_tflite.py

--- convert_to_tflite.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-# Convert the model to TFLite format
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-tflite_model = converter.convert()
-
-# Save the TFLite model
-with open('model.tflite', 'wb') as f:
-    f.write(tflite_model)
